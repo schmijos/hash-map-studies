a30e507f4d3903f0aab06b4fd9e56db64beea7bb
Josua Schmid <josua.schmid@renuo.ch>
2018-10-16 18:23:35 +0200

    Improve readability of main loop

--- optimizers/rename_your_optimizer.rb
+++ optimizers/rename_your_optimizer.rb
@@ -1,6 +1,6 @@
-your_locator = proc do |_key, collision_count|
-  # TODO: your optimizer here
-  collision_count
+your_locator = proc do |key, collision_count|
+  # TODO: your optimizer here, e.g.:
+  key.length + collision_count
 end
 
 OptimizerRepository.register('TODO: fancy name', your_locator)
