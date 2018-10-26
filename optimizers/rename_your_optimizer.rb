af3032a7dea1226aaa476c43c252b60f75697597
Josua Schmid <josua.schmid@renuo.ch>
2018-10-26 22:14:16 +0200

    Add score and comments

--- optimizers/rename_your_optimizer.rb
+++ optimizers/rename_your_optimizer.rb
@@ -1,6 +1,12 @@
+# The following callable gets two parameters:
+# - a string `key` which addresses the entry
+# - an integer `collision_count` which tells you how often the index you returned
+#   for this key pointed to an already occupied storage field
+# It should return an integer which tells the calling code where to find the element belonging to this key.
 your_locator = proc do |key, collision_count|
-  # TODO: your optimizer here, e.g.:
+  # TODO: your optimizer comes here.
   key.length + collision_count
 end
 
+# Here you register your locator globally here so that it can be called by bin/run
 OptimizerRepository.register('TODO: fancy name', your_locator)
